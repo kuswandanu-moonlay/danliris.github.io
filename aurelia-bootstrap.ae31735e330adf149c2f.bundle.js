03aee19839f9b41b8a0ffad149f1aae30634cc25
efrataretailindo <efrata.retailindo@gmail.com>
2020-09-15 01:51:57 +0000

    Automated Deployment

--- aurelia-bootstrap.ae31735e330adf149c2f.bundle.js
+++ aurelia-bootstrap.ae31735e330adf149c2f.bundle.js
@@ -79,7 +79,7 @@
 /******/ 		script.async = true;
 /******/ 		script.timeout = 120000;
 /******/
-/******/ 		script.src = __webpack_require__.p + "" + chunkId + "." + {"0":"4898b9d9412521ea72f8","1":"a482b24c8a4a422a461a","2":"d3ed207cf5496736d6ee"}[chunkId] + ".chunk.js";
+/******/ 		script.src = __webpack_require__.p + "" + chunkId + "." + {"0":"cb1b3b2b7444843afa9c","1":"a482b24c8a4a422a461a","2":"d3ed207cf5496736d6ee"}[chunkId] + ".chunk.js";
 /******/ 		var timeout = setTimeout(onScriptComplete, 120000);
 /******/ 		script.onerror = script.onload = onScriptComplete;
 /******/ 		function onScriptComplete() {
